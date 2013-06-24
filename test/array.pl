f02e541fb4565a0cac97de8fd059658ddd9eb762
Lancer Guo <lancer.guo@oracle.com>
2013-06-24 00:18:48 -0700

    delete testing scripts

--- test/array.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-#print "Lancer says Hi~\n";
-my @arr = ("apple",3,"bnana");
-for (@arr){
-	print "$_\n";
-}
--- test/readfile.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-open my $in, "<", "input.txt" or die "Can't open input.txt: $!";
-my $line = <$in>;
-print $line;
